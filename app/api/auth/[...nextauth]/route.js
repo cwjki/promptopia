b6c31633cedc7840087e5bcfa01ec4d876e49c3a
cwjki <cwjki97@gmail.com>
2023-08-30 18:25:53 +0200

    connection to the database in the sign in function

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import { connectToDB } from "@utils/database";
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -8,8 +10,24 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+
   async session({ session }) {},
-  async signIn({ profile }) {},
+
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+
+      // check if a user already exist
+
+      // if not, create a new user 
+
+      return true;
+      
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
