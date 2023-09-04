9eed3f6d0d613d4e8f352505370ef1cd706f6aa9
cwjki <cwjki97@gmail.com>
2023-09-04 16:10:20 +0200

    create post handler

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,33 +1,51 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 import Form from "@components/Form";
+import { POST } from "@app/api/auth/[...nextauth]/route";
 
 const CreatePrompt = () => {
-
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
 
+  const createPost = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
 
-const createPost = async (e) => {
-
-}
+    try {
+      const response = await fetch("api/prompt/new", {
+        method: POST,
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
 
   return (
-    <Form 
-     type="Create"
-     post={post}
-     setPost={setPost}
-     submitting={submitting}
-     handleSubmit={createPost}
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPost}
     />
-  )
-}
+  );
+};
 
-export default CreatePrompt
+export default CreatePrompt;
