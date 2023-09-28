f3b08ad961cceb8346dcdcc6ff554c36cfa1c098
cwjki <cwjki97@gmail.com>
2023-09-28 13:17:07 +0200

    fixed profile name in the PromptCard

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -43,7 +43,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           />
           <div className="flex flex-col">
             <h3 className="font-satoshi font-semibold text-gray-900">
-              {post.creator.userName}
+              {post.creator.givenName}
             </h3>
             <p className="font-inter text-sm text-gray-500">
               {post.creator.email}
